46b5af6dff17aed90c48e633a59c1af0839ece33
e1082760 <nele2345>
2020-01-30 07:52:53 +0100

    Create order api endpoint

--- cmd/orders-gin-api/main.go
+++ cmd/orders-gin-api/main.go
@@ -2,33 +2,46 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 type Order struct {
-	ID         int `json:"id"`
-	Account    string
-	Instrument string
-	Volume     int
-	Price      float32
+	ID         int     `json:"id"`
+	Account    string  `json:"account"`
+	Instrument string  `json:"instrument"`
+	Volume     int     `json:"volume"`
+	Price      float32 `json:"price"`
 }
 
 var dataStore = map[int]Order{
 	1: {1, "acc", "BMW", 10, 5},
 	2: {2, "acc", "Apple", 12, 6},
+	3: {3, "acc", "Google", 7, 8},
 }
 
 func main() {
 	r := gin.Default()
 
-	v1 := r.Group("/v1/order")
+	v1 := r.Group("/v1/orders")
 	{
 		v1.GET("/", getAllOrders)
 		v1.GET("/:id", getOrderById)
+		v1.POST("/", createOrder)
 	}
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run()
+}
+
+func createOrder(c *gin.Context) {
+	var order Order
+
+	if err := c.BindJSON(&order); err == nil {
+		dataStore[order.ID] = order
+	} else {
+		log.Fatal(err)
+	}
 }
 
 func getOrderById(c *gin.Context) {
@@ -40,7 +53,7 @@ func getOrderById(c *gin.Context) {
 	if ok {
 		c.JSON(http.StatusOK, order)
 	} else {
-		c.JSON(http.StatusNotFound, nil)
+		c.JSON(http.StatusNotFound, "Couldn't be found!")
 	}
 }
 
